d02fdd985a27dfced901a6f505153ad3ae91e8bd
anton <sparco114@ya.ru>
2023-05-01 22:42:43 +0530

    added new model SalesOrder in app 'orders'

--- spd_project/orders/models.py
+++ spd_project/orders/models.py
@@ -1,3 +1,9 @@
+import django.utils.timezone
 from django.db import models
 
-# Create your models here.
+
+class SalesOrder(models.Model):
+    amount = models.IntegerField()
+    description = models.CharField(max_length=255)
+    price = models.IntegerField(default=0)
+    date = models.DateField(default=django.utils.timezone.now)
